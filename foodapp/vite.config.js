a363eee2633cffc9a015a95a9ce2ae902783e047
Eddy <limeddy1125@gmail.com>
2024-06-30 21:40:13 +0800

    deploy foodapp

--- foodapp/vite.config.js
+++ foodapp/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Tailwind-projects/foodapp/"
+  base: "/react-projects/foodapp/"
 })
--- gemini-clone/vite.config.js
+++ gemini-clone/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Tailwind-projects/gemini-clone/"
+  base: "/react-projects/gemini-clone/"
 })
